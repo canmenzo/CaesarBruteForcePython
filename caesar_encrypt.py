c0f0badce6c01e52e98c53352ce8e0e1000c6f4e
Menzo <canozmen@mail.com>
2023-06-19 20:22:55 -0400

    Add files via upload

---
+++ caesar_encrypt.py
@@ -0,0 +1,26 @@
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def encrypt_message(message, key):
+    encrypted = ''
+    for symbol in message:
+        if symbol in LETTERS:
+            num = LETTERS.find(symbol)
+            num = (num + key) % len(LETTERS)
+            encrypted += LETTERS[num]
+        else:
+            encrypted += symbol
+    return encrypted
+
+# Prompt the user for input
+message = input("Enter the message to encrypt: ")
+key = int(input("Enter the encryption key (a non-negative integer): "))
+
+# Encrypt the message
+encrypted_message = encrypt_message(message, key)
+
+# Save the encrypted message to a file
+filename = "encrypted_message.txt"
+with open(filename, "w") as file:
+    file.write(encrypted_message)
+
+print(f"Encrypted message saved to {filename}.")
---
+++ ceaser_decrypt.py
@@ -0,0 +1,20 @@
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# Read the encrypted message from a file
+filename = 'encrypted_message.txt'  # Replace with your file name
+with open(filename, 'r') as file:
+    message = file.read()
+
+# Decrypt the message
+for key in range(len(LETTERS)):
+    translated = ''
+    for symbol in message:
+        if symbol in LETTERS:
+            num = LETTERS.find(symbol)
+            num = num - key
+            if num < 0:
+                num = num + len(LETTERS)
+            translated = translated + LETTERS[num]
+        else:
+            translated = translated + symbol
+    print('Decrypted message #%s: %s' % (key, translated))
